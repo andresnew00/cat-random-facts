96815b3eedfe36977d8334005507b9dd33bce114
andresnew00 <andresnew00@hotmail.com>
2020-05-12 16:02:09 -0400

    added random pictures of cats from api

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,42 @@
-var urlLink = 'https://cat-fact.herokuapp.com/facts/random'
+const factsUrl = "https://cat-fact.herokuapp.com/facts/random";
+const imgUrl = "https://api.thecatapi.com/v1/images/search";
 
-function getRandomFact(url) {
-    return new Promise( (resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.open('GET', urlLink);
+function getResponse(url) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
 
-        request.onload = () => {
-            //checks for the right status
-            if (request.status == 200) {
-                const data = JSON.parse(request.response);
-                document.getElementById("right-section").innerHTML = data.text;
-                resolve(request.response)
-            }
-            // meaningful error 
-            else {
-                reject(Error(request.statusText))
-            }
-        }
+    request.onload = () => {
+      //checks for the right status
+      if (request.status == 200) {
+        const data = JSON.parse(request.response);
+        resolve(data);
+      }
+      // meaningful error
+      else {
+        reject(Error(request.statusText));
+      }
+    };
 
-        //handle network errors
-        request.onerror = () => { 
-            reject(Error("Network Error"))
-        }
+    //handle network errors
+    request.onerror = () => {
+      reject(Error("Network Error"));
+    };
 
-        request.send();
-    } )
+    request.send();
+  });
 }
 
-getRandomFact(urlLink);
+getResponse(factsUrl).then(fact => {
+  document.getElementById("right-section").innerHTML = fact.text;
+});
+
+getResponse(imgUrl).then(img => {
+  var image = document.createElement("img");
+  image.src = img[0].url;
+  image.width = img[0].width;
+  image.height = img[0].height;
+
+  // This next line will just add it to the <body> tag
+  document.getElementById("left-section").appendChild(image);
+});
