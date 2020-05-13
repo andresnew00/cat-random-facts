49899ebfeb2f5063439bae357197b256eec3d44a
andresnew00 <andresnew00@hotmail.com>
2020-05-13 11:22:43 -0400

    styles added

--- scripts/app.js
+++ scripts/app.js
@@ -1,42 +1,42 @@
 const factsUrl = "https://cat-fact.herokuapp.com/facts/random";
 const imgUrl = "https://api.thecatapi.com/v1/images/search";
 
-function getResponse(url) {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.open("GET", url);
-
-    request.onload = () => {
-      //checks for the right status
-      if (request.status == 200) {
-        const data = JSON.parse(request.response);
-        resolve(data);
-      }
-      // meaningful error
-      else {
-        reject(Error(request.statusText));
-      }
-    };
-
-    //handle network errors
-    request.onerror = () => {
-      reject(Error("Network Error"));
-    };
-
-    request.send();
-  });
-}
-
 getResponse(factsUrl).then(fact => {
-  document.getElementById("right-section").innerHTML = fact.text;
+  document.getElementById("right-section").innerHTML = "<p>" + fact.text + "</p>";
 });
 
 getResponse(imgUrl).then(img => {
   var image = document.createElement("img");
   image.src = img[0].url;
-  image.width = img[0].width;
-  image.height = img[0].height;
 
   // This next line will just add it to the <body> tag
   document.getElementById("left-section").appendChild(image);
 });
+
+//Functions
+
+function getResponse(url) {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open("GET", url);
+  
+      request.onload = () => {
+        //checks for the right status
+        if (request.status == 200) {
+          const data = JSON.parse(request.response);
+          resolve(data);
+        }
+        // meaningful error
+        else {
+          reject(Error(request.statusText));
+        }
+      };
+  
+      //handle network errors
+      request.onerror = () => {
+        reject(Error("Network Error"));
+      };
+  
+      request.send();
+    });
+  }
